cb3ba69ae209583a509e69f014150e39dfa15903
Jul <julklingel@juls-mbp.localdomain>
2023-04-14 12:41:37 +0200

    config population

--- main.py
+++ main.py
@@ -247,3 +247,20 @@ def main():
 
 
 main()
+
+
+def run( config_file ):
+    config = neat.config.Config( neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_file )
+
+    p = neat.Population( config )
+    p.add_reporter( neat.StdOutReporter( True ) )
+    stats = neat.StatisticsReporter()
+    p.add_reporter( stats )
+
+    
+
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward.txt')
+    run(config_path)
